72d478b1a09ebb52bcf8eb133cc9983e4286a712
Verma Mukesh <vermamukesh67@gmail.com>
2017-01-18 23:27:27 +0530

    ghost followers

---
+++ InstaInSight/GhostFollowers.h
@@ -0,0 +1,13 @@
+//
+//  GhostFollowers.h
+//  InstaInSight
+//
+//  Created by Verma Mukesh on 18/01/17.
+//  Copyright © 2017 Mukesh Verma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GhostFollowers : UIViewController
+
+@end
---
+++ InstaInSight/GhostFollowers.m
@@ -0,0 +1,37 @@
+//
+//  GhostFollowers.m
+//  InstaInSight
+//
+//  Created by Verma Mukesh on 18/01/17.
+//  Copyright © 2017 Mukesh Verma. All rights reserved.
+//
+
+#import "GhostFollowers.h"
+
+@interface GhostFollowers ()
+
+@end
+
+@implementation GhostFollowers
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
